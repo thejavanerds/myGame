7cecca3ec79bf90519f9fa18e86900356c178911
ZhsCraft <zhscraft@gmail.com>
2015-02-06 04:55:49 -0500

    adde

--- src/com/thejavanerds/zhscraft/myGame/Texture.java
+++ src/com/thejavanerds/zhscraft/myGame/Texture.java
@@ -1,12 +1,15 @@
 package com.thejavanerds.zhscraft.myGame;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL12;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Created by ZhsCraft on 2/6/2015.
@@ -38,14 +41,42 @@ public class Texture {
         for (int y = 0; y < image.getHeight(); y++) {
             for (int x = 0; x < image.getWidth(); x++) {
                 int pixel = pixels[y * image.getWidth() + x];
-                buffer.put((byte) ((pixel >> 16 & 0xFF)));
-                buffer.put((byte) ((pixel >> 8 & 0xff)));
+                buffer.put((byte) ((pixel >> 16) & 0xFF));
+                buffer.put((byte) ((pixel >> 8) & 0xff));
                 buffer.put((byte) ((pixel & 0xFF)));
-                buffer.put((byte) ((pixel >> 24 & 0xFF)));
+                buffer.put((byte) ((pixel >> 24) & 0xFF));
             }
         }
-        return null;
+
+        buffer.flip();
+
+        int id = glGenTextures();
+
+        glBindTexture(GL_TEXTURE_2D, id);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+
+        return new Texture(id, image.getWidth(), image.getHeight());
     }
 
+    public void bind() {
+        glEnable(GL_TEXTURE_2D);
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+
+        glBindTexture(GL_TEXTURE_2D, id);
+
+    }
+
+    public void unbind()
+    {
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    public void delete()
+    {
+        glDeleteTextures(id);
+    }
 }
